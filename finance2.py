8ea361cee1f31419c01866d287dd2ad4ceda8071
WhiteBoar <czarekmeister@gmail.com>
2021-12-15 14:32:30 +0100

    added constant

--- finance2.py
+++ finance2.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 import pandas as pd
 from pandas_datareader import data as web  # Reads stock data
@@ -6,9 +7,26 @@ import matplotlib.pyplot as plt  # Plotting
 import matplotlib.dates as mdates  # Styling dates
 
 import datetime as dt  # For defining dates
+#global variables:
+# Start date defaults
+S_YEAR = 2017
+S_MONTH = 1
+S_DAY = 3
+S_DATE_STR = f"{S_YEAR}-{S_MONTH}-{S_DAY}"
+S_DATE_DATETIME = dt.datetime(S_YEAR, S_MONTH, S_DAY)
 
+# End date defaults
+E_YEAR = 2021
+E_MONTH = 8
+E_DAY = 19
+E_DATE_STR = f"{E_YEAR}-{E_MONTH}-{E_DAY}"
+E_DATE_DATETIME = dt.datetime(E_YEAR, E_MONTH, E_DAY)
+
+folder = "C:/Users/KarolUrbanski/OneDrive - northvolt.com/Documents/Finance Python2/"
 
 import mplfinance as mpf # Matplotlib finance
+
+
 #hold stocks not downloaded
 stock_not_downloaded = []
 missing_stocks = []
@@ -48,7 +66,22 @@ def get_column_from_csv(file, column_name):
         return df[column_name]
 
 tickers = get_column_from_csv(r"C:\Users\KarolUrbanski\Downloads\Wilshire-5000-Stocks-New.csv","Ticker")
-folder = "C:/Users/KarolUrbanski/OneDrive - northvolt.com/Documents/Finance Python2/"
-for x in range (2):
-    save_to_csv_from_yahoo(folder,tickers[x],2017,1,1,2020,1,1)
-    print("done!")
+
+for x in range (500):
+    save_to_csv_from_yahoo(folder,tickers[x],2017,1,1,2021,1,1)
+print("finished")
+
+
+for x in stock_not_downloaded:
+    save_to_csv_from_yahoo(folder,x,2017,1,1,2021,1,1)
+print("done")
+print(stock_not_downloaded)
+
+files = [x for x in listdir(folder) if isfile(join(folder, x))]
+
+# Remove extension from file names
+# Splitext splits the file name into 2 parts being the name and extension
+# We say get all file names and then store just the name in our list named files
+tickers = [os.path.splitext(x)[0] for x in files]
+tickers
+
